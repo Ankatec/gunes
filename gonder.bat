d5d5276e789e459242760eb60cc57ff6c8ef082c
Ankara Tecno <ankara.tecno@technologist.com>
2026-05-03 18:08:46 +0300

    GunesOS Guncelleme

--- gonder.bat
+++ gonder.bat
@@ -1,10 +1,17 @@
 @echo off
 title GitHub Guncelleyici - GunesOS
-echo Dosyalar GitHub'a ZORLA gonderiliyor...
+echo Dosyalar GitHub'a gonderiliyor...
+
+REM dist klasorunu Git takibinden kaldir
+git rm -r --cached dist 2>nul
+
+REM Tum dosyalari ekle (.gitignore kurallarina uyulur)
 git add .
-git commit -m "GunesOS Kesin Guncelleme"
-:: --force komutu ile engelleri asıyoruz
-git push origin main --force
+
+REM Commit ve push
+git commit -m "GunesOS Guncelleme"
+git push origin main
+
 echo.
 echo Islem TAMAM! Simdi GitHub Actions sekmesini kontrol et.
 pause
